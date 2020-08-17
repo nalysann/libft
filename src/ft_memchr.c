06a25797a5f4119a0f75fda734df8118a628e88d
nalysann <urb-ilya@yandex.ru>
2020-08-17 16:36:06 +0300

    reworked strlen, fixed memchr and strchr

--- src/ft_memchr.c
+++ src/ft_memchr.c
@@ -12,56 +12,47 @@
 
 #include <stddef.h>
 
-static void	*align_to_lword(const unsigned char **ptr, unsigned char uc,
-							size_t *n)
+static void		*search_with_longwords(const unsigned char *char_ptr,
+										unsigned char uc, size_t n)
 {
-	const unsigned char	*char_ptr;
+	const unsigned long		*long_ptr;
+	unsigned long			longword;
+	unsigned long			mask;
 
-	char_ptr = *ptr;
-	while (*n && ((size_t)char_ptr & (sizeof(unsigned long) - 1)))
-	{
-		if (*char_ptr == uc)
-			return ((void *)char_ptr);
-		++char_ptr;
-		--*n;
-	}
-	*ptr = char_ptr;
-	return (NULL);
-}
-
-static void	*check_end(const unsigned char *char_ptr, unsigned char uc,
-						size_t n)
-{
-	while (n--)
-	{
-		if (*char_ptr == uc)
-			return ((void *)char_ptr);
-		++char_ptr;
-	}
-	return (NULL);
-}
-
-void		*ft_memchr(const void *s, int c, size_t n)
-{
-	const unsigned char	*char_ptr;
-	const unsigned long	*long_ptr;
-	unsigned char		uc;
-	unsigned long		repeated_uc;
-	unsigned long		lword;
-
-	uc = (unsigned char)c;
-	char_ptr = (const unsigned char *)s;
-	align_to_lword(&char_ptr, uc, &n);
-	long_ptr = (const unsigned long *)char_ptr;
-	repeated_uc = 0x0101010101010101 * (unsigned long)uc;
+	long_ptr = (unsigned long *)char_ptr;
+	mask = 0x0101010101010101 * (unsigned long)uc;
 	while (n >= sizeof(unsigned long))
 	{
-		lword = *long_ptr ^ repeated_uc;
-		if ((lword - 0x0101010101010101) & ~lword & 0x8080808080808080)
+		longword = *long_ptr ^ mask;
+		if ((longword - 0x0101010101010101) & ~longword & 0x8080808080808080)
 			break ;
 		++long_ptr;
 		n -= sizeof(unsigned long);
 	}
 	char_ptr = (const unsigned char *)long_ptr;
-	return (check_end(char_ptr, uc, n));
+	while (n > 0)
+	{
+		if (*char_ptr == uc)
+			return ((void *)char_ptr);
+		--n;
+		++char_ptr;
+	}
+	return (NULL);
+}
+
+void			*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char		*char_ptr;
+	unsigned char			uc;
+
+	uc = (unsigned char)c;
+	char_ptr = (const unsigned char *)s;
+	while (n > 0 && ((size_t)char_ptr & (sizeof(unsigned long) - 1)))
+	{
+		if (*char_ptr == uc)
+			return ((void *)char_ptr);
+		--n;
+		++char_ptr;
+	}
+	return (search_with_longwords(char_ptr, uc, n));
 }
--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -12,60 +12,37 @@
 
 #include <stddef.h>
 
-static const char	*align_to_lword(const char **ptr)
+static int	count_with_longwords(const char *s, const char *char_ptr)
+{
+	const unsigned long		*long_ptr;
+	unsigned long			longword;
+	size_t					i;
+
+	long_ptr = (unsigned long *)char_ptr;
+	while (1)
+	{
+		longword = *long_ptr++;
+		if (((longword - 0x0101010101010101) & ~longword & 0x8080808080808080))
+		{
+			char_ptr = (const char *)(long_ptr - 1);
+			i = -1;
+			while (++i < sizeof(unsigned long))
+				if (char_ptr[i] == '\0')
+					return (char_ptr - s + i);
+		}
+	}
+}
+
+size_t		ft_strlen(const char *s)
 {
 	const char	*char_ptr;
 
-	char_ptr = *ptr;
+	char_ptr = (const char *)s;
 	while ((size_t)char_ptr & (sizeof(unsigned long) - 1))
 	{
 		if (*char_ptr == '\0')
-			return (char_ptr);
+			return (char_ptr - s);
 		++char_ptr;
 	}
-	*ptr = char_ptr;
-	return (NULL);
-}
-
-static const char	*check_zero(const unsigned long *lword)
-{
-	const char	*char_ptr;
-
-	char_ptr = (const char *)lword;
-	if (char_ptr[0] == '\0')
-		return (char_ptr);
-	if (char_ptr[1] == '\0')
-		return (char_ptr + 1);
-	if (char_ptr[2] == '\0')
-		return (char_ptr + 2);
-	if (char_ptr[3] == '\0')
-		return (char_ptr + 3);
-	if (char_ptr[4] == '\0')
-		return (char_ptr + 4);
-	if (char_ptr[5] == '\0')
-		return (char_ptr + 5);
-	if (char_ptr[6] == '\0')
-		return (char_ptr + 6);
-	if (char_ptr[7] == '\0')
-		return (char_ptr + 7);
-	return (NULL);
-}
-
-size_t				ft_strlen(const char *s)
-{
-	const char			*s_char;
-	const unsigned long	*s_long;
-	const char			*ret;
-
-	s_char = s;
-	if ((ret = align_to_lword(&s_char)))
-		return (ret - s);
-	s_long = (const unsigned long *)s_char;
-	while (1)
-	{
-		if ((*s_long - 0x0101010101010101) & ~*s_long & 0x8080808080808080)
-			if ((ret = check_zero(s_long)))
-				return (ret - s);
-		++s_long;
-	}
+	return (count_with_longwords(s, char_ptr));
 }
