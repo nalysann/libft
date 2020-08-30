41fcd94a98d6109b0e634542f76ec07107641726
nalysann <urbilya@gmail.com>
2020-08-30 12:19:25 +0300

    fixed digittoint

--- src/ft_digittoint.c
+++ src/ft_digittoint.c
@@ -15,9 +15,9 @@ int		ft_digittoint(int c)
 	if ('0' <= c && c <= '9')
 		return (c - '0');
 	else if ('a' <= c && c <= 'f')
-		return (c - 'a');
+		return (10 + c - 'a');
 	else if ('A' <= c && c <= 'F')
-		return (c - 'A');
+		return (10 + c - 'A');
 	else
-		return (-1);
+		return (0);
 }
