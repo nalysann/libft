42dbdba481daecd879ae483ff5cc5c2b5cb5bb00
Ilia Urbanovich <urbilya@gmail.com>
2021-03-13 16:02:58 +0300

    Updated stdlib header

--- include/ft_stdlib.h
+++ include/ft_stdlib.h
@@ -20,11 +20,14 @@ int			ft_abs(int i);
 int			ft_atoi(const char *str);
 long		ft_atol(const char *str);
 long long	ft_atoll(const char *str);
+size_t		ft_intlen(int n);
 char		*ft_itoa(int n);
 long		ft_labs(long i);
 long long	ft_llabs(long long i);
 void		*ft_memalloc(size_t size);
 void		ft_memdel(void **ap);
+void		ft_qsort(void *data, size_t len, size_t size,
+					int (*cmp)(const void *, const void *));
 void		ft_strdel(char **as);
 char		*ft_strnew(size_t size);
 long		ft_strtol(const char *str, char **endptr, int base);
